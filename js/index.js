d3edd662f626ce9d26fb046ef1b8853fafc197ac
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-09-20 20:07:02 -0700

    finished lesson 4.2

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+let today = new Date();
+let thisYear = today.getFullYear();
+
+const footer = document.querySelector('footer');
+//footer.innerHTML = thisYear;
+
+const copyright = document.createElement('p');
+copyright.innerHTML  = "&copy; Lawrence " + thisYear;
+
+footer.appendChild(copyright);
+
+let skills = ["Javascript", "HTML", "CSS", "Java", "Python", "C"];
+const skillsSection = document.querySelector('#skills');
+
+let skillsList = skillsSection.querySelector('ul');
+
+for(let i = 0; i < skills.length; i++){
+	let li = document.createElement('li');
+	li.innerHTML = skills[i];
+	skillsList.appendChild(li);
+}
